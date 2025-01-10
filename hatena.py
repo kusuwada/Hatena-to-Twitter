36ff33cc4a8aa99767fc2071e5dec10d27456be7
kusuwada <kusuwadan@gmail.com>
2025-01-10 15:18:25 +0900

    Update hatena.py

--- hatena.py
+++ hatena.py
@@ -107,7 +107,7 @@ class Hatena:
             links = self.select_elements_of_tag(root, '{http://www.w3.org/2005/Atom}link')
             print(links)
             for link in links:
-                    print(link.attrib)
+                print(link.attrib)
                 if 'next' in link.attrib:
                     print(link.attrib['href'])
             
